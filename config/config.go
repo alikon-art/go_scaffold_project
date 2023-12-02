804f420e6d96583fa82ec27ca6b4c009c947ac1f
alikon-art <alikon-art@users.noreply.github.com>
2023-12-02 20:34:14 +0800

    test

--- config/config.go
+++ config/config.go
@@ -1,5 +1,59 @@
 package config
 
+import (
+	"fmt"
+	"go_scaffold_project/utils/logs"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var Port string
+
+func logsinit() {
+	logLevel := viper.GetString("logs.level")
+	// 设置日志级别
+	switch logLevel {
+	case "trace":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+	default:
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logs.Info(nil, fmt.Sprint("Set log level to : ", logrus.GetLevel()))
+	// 日志中显示文件名
+	SetReportCaller := viper.GetBool("logs.showcaller")
+	logrus.SetReportCaller(SetReportCaller)
+	// 时间显示格式
+	TimestampFormat := viper.GetString("logs.timeformat")
+	logrus.SetFormatter(&logrus.JSONFormatter{TimestampFormat: TimestampFormat})
+
+}
+
+func configload() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		logs.Error(nil, "Filed to read config")
+	}
+}
+
+func gininit() {
+	///todo
+	Port = viper.GetString("program.port")
+}
 func init() {
 	// 加载程序配置
+	logs.Debug(nil, "Config init...")
+	configload()
+	logsinit()
+	gininit()
+	logs.Debug(nil, "Config init completed")
 }
--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"go_scaffold_project/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", getUserInfo)
-	apiGroup := r.Group("/api")
-	apiGroup.Use(golableMiddleWare)
-	userGroup := apiGroup.Group("/user")
-	userGroup.GET("/login", partMiddleWare, getUserInfo_rawData)
-	userGroup.GET("/logout")
-	// productGroup := apiGroup.Group("/product")
-	r.Run()
+	r.Run(config.Port)
 
 }
